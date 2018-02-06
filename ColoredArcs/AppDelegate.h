e8b5d92d9f8e77d14283bf9854ea98083ca32217
Robert M. Ryan <robert.ryan@mindspring.com>
2018-02-05 23:44:51 -0800

    Initial Commit

---
+++ ColoredArcs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColoredArcs
+//
+//  Created by Robert Ryan on 2/5/18.
+//  Copyright © 2018 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
